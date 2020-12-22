c7ff2ef0c8fb55811289a52ef58393e3658c0f1d
Derek <mail@derek.tech>
2020-12-22 23:04:20 +0000

    Started on submitting

--- client/src/Lobby.jsx
+++ client/src/Lobby.jsx
@@ -1,34 +1,85 @@
 import React from 'react';
 
 class Lobby extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerName: "",
+      roomCode: ""
+    };
+  }
+
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    })
+  };
+
+  handleDummySubmit = (event) => {
+    event.preventDefault();
+  };
+
+  handleCreatePrivateGame = (event) => {
+    event.preventDefault();
+
+    console.log("Creating private game");
+  };
+
   render() {
     return (
-      <div class="container">
-        <div class="card mt-3">
-          <h1 class="card-header">Boss Fight</h1>
-          <div class="card-body">
-            <form>
-              <div class="mb-4">
-                <label class="form-label">Player Name</label>
-                <input type="text" class="form-control"/>
+      <div className="container">
+        <div className="card mt-3">
+          <h1 className="card-header">Boss Fight</h1>
+          <div className="card-body">
+            <form onSubmit={this.handleDummySubmit}>
+              <div className="mb-4">
+                <label className="form-label">Player Name</label>
+                <input
+                  required
+                  name="playerName"
+                  value={this.state.playerName}
+                  onChange={this.handleChange}
+                  type="text"
+                  className="form-control"
+                  placeholder="Mr. Woshy" />
               </div>
               <div className="mt-4 mb-4">
                 <label className="form-label">Room Code</label>
                 <div className="input-group mb-3">
-                  <input type="text" className="form-control" placeholder="BIGBAL"/>
-                  <button className="btn btn-secondary" type="button" id="button-addon1">Join game</button>
+                  <input
+                    name="roomCode"
+                    value={this.state.roomCode}
+                    onChange={this.handleChange}
+                    type="text"
+                    className="form-control"
+                    placeholder="BIGBAL" />
+                  <button
+                    className="btn btn-secondary">
+                    Join game
+                  </button>
                 </div>
               </div>
               <hr/>
-              <div className="row mt-4">
+              <div className="row mt-4 mb-1">
                 <div className="col text-center">
                   <div className="d-grid">
-                    <button className="btn btn-primary">Create public game</button>
+                    <button
+                      className="btn btn-primary">
+                      Create public game
+                    </button>
                   </div>
                 </div>
                 <div className="col text-center">
                   <div className="d-grid">
-                    <button className="btn btn-outline-secondary btn-block">Create private game</button>
+                    <button
+                      onClick={this.handleCreatePrivateGame}
+                      className="btn btn-outline-secondary btn-block">
+                      Create private game
+                    </button>
                   </div>
                 </div>
               </div>
